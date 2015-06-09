d4fe46fefa7e3b6549905290ffd5933b33ac77cc
Ryan Springer <ryanespringer-at-gmail-dot-com>
2015-06-09 17:58:18 +0200

    Added setup.sh for dev ubuntu

---
+++ dev-ubuntu/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir ~/bin
+cp .vimrc ~/.vimrc
+cp .bashrc ~/.bashrc
+cp .gitconfig ~/.gitconfig
+
+# Set up vim-plug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Done
+
+echo "All done, please: source ~/.bashrc"
