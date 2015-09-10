5b952d3e524379dc5dac6dbcc06565e7bde94d07
Ryan Springer <ryan.springer@datastax.com>
2015-09-10 16:58:18 +0000

    added lein profiles

--- dev-ubuntu/.lein/profiles.clj
+++ dev-ubuntu/.lein/profiles.clj
@@ -1,7 +1,6 @@
-{:user
- {:plugins
-   ;; [[refactor-nrepl "1.0.5"]
-;;    [[refactor-nrepl "1.1.0-SNAPSHOT"]
-     [[cider/cider-nrepl "0.9.0"]]
-  :dependencies 
-    [[org.clojure/tools.nrepl "0.2.10"]]}}
+;; remember to use clojure 1.7 and not clojure 1.6 for clj-refactor to work
+{:user {
+ :plugins [[refactor-nrepl "1.2.0-SNAPSHOT"]
+           [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
+ :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+
