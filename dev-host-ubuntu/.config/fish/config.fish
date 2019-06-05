fcb06f1ead9a7d19fa6bae8d44de69656e47ecf1
Ryan Springer <ryan.springer@datastax.com>
2019-06-05 19:07:25 +0200

    graalvm

--- dev-host-ubuntu/.config/fish/config.fish
+++ dev-host-ubuntu/.config/fish/config.fish
@@ -1,3 +1,6 @@
 export GOPATH=/home/ryan/go
+export GRAAL_HOME=/home/ryan/graalvm-ce-19.0.0
+export KUBEBUILDER_CONTROLPLANE_START_TIMEOUT=120
+export KUBEBUILDER_CONTROLPLANE_STOP_TIMEOUT=120
 set PATH $PATH $GOPATH/bin
 set PATH $PATH /usr/local/kubebuilder/bin
