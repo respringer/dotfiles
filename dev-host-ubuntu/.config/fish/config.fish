2ead7522276376554056f7aed66afec4737ac7f0
Ryan Springer <ryan.springer@datastax.com>
2019-08-26 15:09:13 +0200

    google sdk

--- dev-host-ubuntu/.config/fish/config.fish
+++ dev-host-ubuntu/.config/fish/config.fish
@@ -3,4 +3,13 @@ export GRAAL_HOME=/home/ryan/graalvm-ce-19.0.0
 export KUBEBUILDER_CONTROLPLANE_START_TIMEOUT=120
 export KUBEBUILDER_CONTROLPLANE_STOP_TIMEOUT=120
 set PATH $PATH $GOPATH/bin
+set PATH $PATH $GRAAL_HOME/bin
 set PATH $PATH /usr/local/kubebuilder/bin
+#export KUBECONFIG=(kind get kubeconfig-path --name="kind")
+set -x PATH "/home/ryan/.pyenv/bin" $PATH
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/ryan/google-cloud-sdk/path.fish.inc' ]; . '/home/ryan/google-cloud-sdk/path.fish.inc'; end
